8e36c12ee0444a80387537c1554e1f9c92ac8a2c
Pasi Niemi <pasi.niemi@nitorcreations.com>
2016-05-12 12:54:45 +0300

    Add script to show logs

---
+++ slss-workshop/show-function-logs.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+usage() {
+  if [ "$1" ]; then
+    echo "$1"
+  fi
+  echo "usage: $0 <function-name> "
+  exit 1
+}
+
+if [ "$1" ]; then
+  FUNCTION="$1"
+else
+  usage
+fi
+
+PROJECT=$(for i in _meta/variables/s-variables-*.json; do jq -r '.|select(.project != null)|.project' < $i; done)
+STAGE=$(for i in _meta/variables/s-variables-*.json; do jq -r '.|select(.stage != null)|.stage' < $i; done)
+VERSION=$(aws lambda get-alias --function-name $PROJECT-$FUNCTION --name $STAGE  | jq -r '.FunctionVersion')
+
+if STREAM=$(aws logs describe-log-streams --log-group-name "/aws/lambda/$PROJECT-$FUNCTION" | jq -r ".logStreams[].logStreamName" | grep "$(date +%Y/%m/%d/)\[$VERSION\]"); then
+  aws logs get-log-events --log-group-name "/aws/lambda/$PROJECT-$FUNCTION" --log-stream-name $STREAM | jq -r '.events[].message' | grep -v "^$"
+else
+  echo "No stream found"
+  exit 1
+fi
